6fec2dc2a255f4ac7624438c39b09f62b9a3b2c6
tiatiahwang <tiahwang44@gmail.com>
2023-04-30 00:59:35 -0700

    create: text editor

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import '@/styles/globals.css';
+import 'react-quill/dist/quill.snow.css';
 import { SWRConfig } from 'swr';
 
 export default function App({ Component, pageProps }) {
---
+++ pages/posts/upload.js
@@ -0,0 +1,120 @@
+import Button from '@/components/button';
+import dynamic from 'next/dynamic';
+import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
+import { useForm } from 'react-hook-form';
+
+const QuillNoSSRWrapper = dynamic(import('react-quill'), {
+  ssr: false,
+  loading: () => <p>Loading ...</p>,
+});
+
+export default function Upload() {
+  const quillRef = useRef();
+  const inputRef = useRef();
+  const [contents, setContents] = useState('');
+
+  const imageHandler = useCallback(() => {
+    // 1. 이미지를 저장할 input type=file DOM을 만든다.
+    const input = document.createElement('input');
+    console.log('실행');
+    input.setAttribute('type', 'file');
+    input.setAttribute('accept', 'image/*'); // 이미지 파일만 선택가능하도록 제한
+    input.setAttribute('name', 'image');
+    input.click(); // 에디터의 이미지버튼을 클릭하면 이 input이 클릭된다.
+
+    // input이 클릭되면 파일 선택창이 화면에 나타난다.
+    // 파일 선택창에서 이미지를 선택하면 실행될 콜백 함수 등록
+    // input.onchange = async () => {
+    //   const file = input.files[0];
+    //   // multer에 맞는 형식으로 데이터를 만들어 준다
+    //   // 이미지를 url로 바꾸기 위해 서버로 전달하는 폼데이터를 만드는 것이다.
+    //   const formData = new FormData();
+    //   formData.append('image', file); // formData는 키-밸류 구조
+
+    //   try {
+    //     // 폼데이터를 서버에 넘겨 multer로 이미지 URL 받아오기
+    //     const result = await axios.post(
+    //       `${process.env.REACT_APP_API_URL}/uploadimg`,
+    //       formData,
+    //     );
+    //     if (!result.data.url) {
+    //       alert('이미지 업로드에 실패하였습니다.');
+    //     }
+    //     // 이 URL을 img 태그의 src에 넣은 요소를 현재 에디터의 커서에 넣어주면 에디터 내에서 이미지가 나타난다
+    //     // src가 base64가 아닌 짧은 URL이기 때문에 데이터베이스에 에디터의 전체 글 내용을 저장할 수있게된다
+    //     // 이미지는 꼭 로컬 백엔드 uploads 폴더가 아닌 다른 곳에 저장해 URL로 사용하면된다.
+
+    //     const editor = quillRef.current.getEditor(); // 에디터 객체 가져오기
+    //     const range = editor.getSelection(); // 커서 위치에 이미지 삽입
+    //     editor.insertEmbed(range.index, 'image', result.data.url);
+    //   } catch (e) {
+    //     console.log(e);
+    //   }
+    // }
+  }, [quillRef]);
+
+  const modules = useMemo(
+    () => ({
+      toolbar: {
+        // 툴바에 넣을 기능들을 순서대로 나열하면 된다.
+        container: [
+          ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+          [{ size: ['small', false, 'large', 'huge'] }, { color: [] }],
+          [
+            { list: 'ordered' },
+            { list: 'bullet' },
+            { indent: '-1' },
+            { indent: '+1' },
+            { align: [] },
+          ],
+          ['image', 'video'],
+        ],
+        handlers: {
+          // 위에서 만든 이미지 핸들러 사용하도록 설정
+          image: imageHandler,
+        },
+      },
+    }),
+    [imageHandler],
+  );
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const title = document.querySelector('input').value;
+
+    const post = {
+      title,
+      contents,
+      category: 'Q&A',
+    };
+
+    console.log(post);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div className='flex items-center justify-start'>
+          <span>제목</span>
+          <input
+            ref={inputRef}
+            type='text'
+            placeholder='제목을 입력해주세요'
+            className='w-appearance-none px-3 py-2 placeholder-gray-400 focus:outline-none'
+          />
+        </div>
+        <QuillNoSSRWrapper
+          ref={quillRef}
+          value={contents}
+          onChange={setContents}
+          modules={modules}
+          theme='snow'
+        />
+        <div className='pt-5'>
+          <Button text={'글 작성하기'} />
+        </div>
+      </form>
+    </div>
+  );
+}
