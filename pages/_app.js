160d09f05133ca2e21aee71acb1295919d678826
tiatiahwang <tiahwang44@gmail.com>
2023-05-14 22:12:46 -0700

    fix: full bg

--- pages/_app.js
+++ pages/_app.js
@@ -10,8 +10,10 @@ export default function App({ Component, pageProps }) {
           fetch(url).then((response) => response.json()),
       }}
     >
-      <div className='w-full mx-auto max-w-7xl text-black dark:bg-darkbg dark:text-white'>
-        <Component {...pageProps} />
+      <div className='min-h-screen w-full text-black dark:bg-darkbg dark:text-white'>
+        <div className='mx-auto max-w-7xl'>
+          <Component {...pageProps} />
+        </div>
       </div>
     </SWRConfig>
   );
