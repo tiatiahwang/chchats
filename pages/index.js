2c74db43edc6ea50f8aacada84251ebe3bf72465
tiatiahwang <tiahwang44@gmail.com>
2023-05-27 20:12:03 -0700

    add: responsive css on Home screen

--- pages/index.js
+++ pages/index.js
@@ -19,21 +19,23 @@ export default function Home() {
   // PostList에 category 정보 보내는 방식 변경 추후 필요할 수도
   return (
     <Layout title='홈' noPaddingTop={true}>
-      <PostList
-        category={categories[0]}
-        data={QuestionsData}
-        isHome={true}
-      />
-      <PostList
-        category={categories[1]}
-        data={InfoData}
-        isHome={true}
-      />
-      <PostList
-        category={categories[2]}
-        data={MarketData}
-        isHome={true}
-      />
+      <div className='min-w-[500px] md:w md:grid md:gap-6 md:grid-cols-2'>
+        <PostList
+          category={categories[0]}
+          data={QuestionsData}
+          isHome={true}
+        />
+        <PostList
+          category={categories[1]}
+          data={InfoData}
+          isHome={true}
+        />
+        <PostList
+          category={categories[2]}
+          data={MarketData}
+          isHome={true}
+        />
+      </div>
     </Layout>
   );
 }
