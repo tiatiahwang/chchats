aeab06e019a812f6fa21c2724db37a8f9c63854f
tiatiahwang <tiahwang44@gmail.com>
2023-05-27 20:04:59 -0700

    fix: load data on Home screen

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Layout from '@/components/layout';
 import PostList from '@/components/postList';
+import { categories } from '@/libs/client/utils';
 import { useRouter } from 'next/router';
 import useSWR from 'swr';
 
@@ -8,21 +9,28 @@ export default function Home() {
   const { data: QuestionsData } = useSWR(
     '/api/posts?category=questions',
   );
+  const { data: InfoData } = useSWR(
+    '/api/posts?category=information',
+  );
   const { data: MarketData } = useSWR(
     '/api/posts?category=market',
   );
 
+  // PostList에 category 정보 보내는 방식 변경 추후 필요할 수도
   return (
     <Layout title='홈' noPaddingTop={true}>
       <PostList
-        categoryName='Q&A'
-        onClick={() => router.push('/questions/new')}
+        category={categories[0]}
         data={QuestionsData}
         isHome={true}
       />
       <PostList
-        categoryName='Market'
-        onClick={() => router.push('/market/new')}
+        category={categories[1]}
+        data={InfoData}
+        isHome={true}
+      />
+      <PostList
+        category={categories[2]}
         data={MarketData}
         isHome={true}
       />
