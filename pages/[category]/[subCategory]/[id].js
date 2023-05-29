2f5151d232e9979f95eb00844b696b3aa049b604
tiatiahwang <tiahwang44@gmail.com>
2023-05-28 18:53:35 -0700

    edit: detail screen css

--- pages/[category]/[subCategory]/[id].js
+++ pages/[category]/[subCategory]/[id].js
@@ -2,10 +2,8 @@ import Category from '@/components/category';
 import Comments from '@/components/comments';
 import Layout from '@/components/layout';
 import PostDetail from '@/components/postDetail';
-import {
-  categories,
-  questionsCategories,
-} from '@/libs/client/utils';
+import { categories } from '@/libs/client/utils';
+import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { useLayoutEffect, useState } from 'react';
 import useSWR from 'swr';
@@ -31,18 +29,16 @@ export default function DetailPage() {
   return (
     <Layout>
       <div className='p-4 space-y-6'>
-        <div className='dark:text-white bg-gray-200 dark:bg-darkselected py-4 px-8 rounded-md'>
-          <p className='font-semibold text-md'>
-            {mainCategory.name}
-          </p>
-          <p className='text-xs'>
-            {mainCategory.description}
-          </p>
-        </div>
-        <Category
-          categories={questionsCategories}
-          selected={data?.post?.subCategory}
-        />
+        <Link href={`/${mainCategory.ref}`}>
+          <div className='dark:text-white bg-gray-200 dark:bg-darkselected py-4 px-8 rounded-md'>
+            <p className='font-semibold text-md'>
+              {mainCategory.name}
+            </p>
+            <p className='text-xs'>
+              {mainCategory.description}
+            </p>
+          </div>
+        </Link>
         <PostDetail post={data?.post} />
         {data?.post?.comments.length > 0 ? (
           <Comments comments={data?.post?.comments} />
