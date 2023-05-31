78dec77638adcbdf950e65c5195a24894c153da0
tiatiahwang <tiahwang44@gmail.com>
2023-05-30 21:54:36 -0700

    create: loader component

---
+++ components/loader.js
@@ -0,0 +1,3 @@
+export default function Loader() {
+  return <div className='text-2xl'>로딩중</div>;
+}
--- pages/[category]/[subCategory]/[id].js
+++ pages/[category]/[subCategory]/[id].js
@@ -1,5 +1,6 @@
 import Comments from '@/components/comments';
 import Layout from '@/components/layout';
+import Loader from '@/components/loader';
 import Modal from '@/components/modal';
 import PostDetail from '@/components/postDetail';
 import useMutation from '@/libs/client/useMutation';
@@ -19,7 +20,7 @@ export default function DetailPage() {
   const { user } = useUser();
   const router = useRouter();
   const { register, handleSubmit, reset } = useForm();
-  const { data, mutate } = useSWR(
+  const { data, mutate, isLoading } = useSWR(
     router.query.id
       ? `/api/posts/${router.query.id}`
       : null,
@@ -65,58 +66,66 @@ export default function DetailPage() {
   return (
     <Layout>
       <div className='p-4 space-y-6'>
-        {/* 메인 카테고리 */}
-        <Link href={`/${mainCategory.ref}`}>
-          <div className='dark:text-white bg-gray-200 dark:bg-darkselected py-4 px-8 rounded-md'>
-            <p className='font-semibold text-md'>
-              {mainCategory.name}
-            </p>
-            <p className='text-xs'>
-              {mainCategory.description}
-            </p>
-          </div>
-        </Link>
-        {/* 글 내용 상세 */}
-        <PostDetail post={data?.post} />
-        {/* 댓글 작성 */}
-        <button
-          onClick={onClickAddComment}
-          className='hover:text-indigo-500 text-sm pb-6'
-        >
-          {showCommentForm ? '댓글창 닫기' : '댓글 쓰기'}
-        </button>
-        {showCommentForm ? (
-          <form
-            className='flex mb-4 gap-4'
-            onSubmit={handleSubmit(onValid)}
-          >
-            <div className='shrink-0'>
-              {user?.avatar ? (
-                <img
-                  src={`https://imagedelivery.net/AjL7FiUUKL0mNbF_IibCSA/${user?.avatar}/avatar`}
-                  className='h-8 w-8 rounded-full'
-                />
-              ) : (
-                <div className='h-8 w-8 rounded-full bg-indigo-100' />
-              )}
-            </div>
-            <textarea
-              {...register('contents')}
-              className='resize-none rounded-md flex-1 focus:outline-none dark:bg-darkbg border-[1px] dark:border-white p-2'
-              placeholder={
-                !user ? '로그인을 해주세요' : null
-              }
-              disabled={!user ? true : false}
-            />
-            <button className='text-sm cursor-pointer hover:text-indigo-500'>
-              댓글 작성
+        {isLoading ? (
+          <Loader />
+        ) : (
+          <>
+            {/* 메인 카테고리 */}
+            <Link href={`/${mainCategory.ref}`}>
+              <div className='dark:text-white bg-gray-200 dark:bg-darkselected py-4 px-8 rounded-md'>
+                <p className='font-semibold text-md'>
+                  {mainCategory.name}
+                </p>
+                <p className='text-xs'>
+                  {mainCategory.description}
+                </p>
+              </div>
+            </Link>
+            {/* 글 내용 상세 */}
+            <PostDetail post={data?.post} />
+            {/* 댓글 작성 */}
+            <button
+              onClick={onClickAddComment}
+              className='hover:text-indigo-500 text-sm pb-6'
+            >
+              {showCommentForm
+                ? '댓글창 닫기'
+                : '댓글 쓰기'}
             </button>
-          </form>
-        ) : null}
-        {/* 댓글 리스트 */}
-        {data?.post?.comments.length > 0 ? (
-          <Comments comments={data?.post?.comments} />
-        ) : null}
+            {showCommentForm ? (
+              <form
+                className='flex mb-4 gap-4'
+                onSubmit={handleSubmit(onValid)}
+              >
+                <div className='shrink-0'>
+                  {user?.avatar ? (
+                    <img
+                      src={`https://imagedelivery.net/AjL7FiUUKL0mNbF_IibCSA/${user?.avatar}/avatar`}
+                      className='h-8 w-8 rounded-full'
+                    />
+                  ) : (
+                    <div className='h-8 w-8 rounded-full bg-indigo-100' />
+                  )}
+                </div>
+                <textarea
+                  {...register('contents')}
+                  className='resize-none rounded-md flex-1 focus:outline-none dark:bg-darkbg border-[1px] dark:border-white p-2'
+                  placeholder={
+                    !user ? '로그인을 해주세요' : null
+                  }
+                  disabled={!user ? true : false}
+                />
+                <button className='text-sm cursor-pointer hover:text-indigo-500'>
+                  댓글 작성
+                </button>
+              </form>
+            ) : null}
+            {/* 댓글 리스트 */}
+            {data?.post?.comments.length > 0 ? (
+              <Comments comments={data?.post?.comments} />
+            ) : null}
+          </>
+        )}
       </div>
       {/* 로그인 안내 모달창 */}
       {showModal ? (
