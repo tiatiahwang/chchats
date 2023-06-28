c8c4b9ecfe440ab815476f46253a04c0b1f093f1
tiatiahwang <tiahwang44@gmail.com>
2023-06-27 20:21:01 -0700

    fix: print profile page and message even when no posts

--- pages/api/users/me/posts.js
+++ pages/api/users/me/posts.js
@@ -32,7 +32,10 @@ async function handler(req, res) {
   res.json({
     ok: true,
     posts,
-    totalPages: Math.ceil(count / limit),
+    totalPages:
+      Math.ceil(count / limit) === 0
+        ? 1
+        : Math.ceil(count / limit),
   });
 }
 
